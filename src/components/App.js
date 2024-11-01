f5464866a64b30f1caf14ade4af5378d76f3eca2
MeghanaSubramanyam <megha@gmail.com>
2024-11-01 22:08:15 +0530

    changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [state, setState] = useState(0);
+
+  function handleChange() {
+    setState((prevState) => prevState + 1);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Button clicked {state} times</p>
+      <button onClick={handleChange}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
